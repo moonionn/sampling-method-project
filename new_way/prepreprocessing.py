a460f826b510f6fb475bc404b61a61d4a9a969db
moonionn <sisisibibi@gmail.com>
2024-06-06 01:25:55 +0800

    整理 程式碼

--- new_way/prepreprocessing.py
+++ new_way/prepreprocessing.py
@@ -1,11 +1,8 @@
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
-# wine_df = pd.read_csv('../newdataset/new_winequality.csv')
 def load_data(df, target_column):
     X = df.drop(columns=[target_column])
     y = df[target_column]
 
     return X, y
-
-# print(load_and_scale_data(wine_df, 'quality'))
